69ee324513629fb3382855e2a04ae44fa6c14333
Renata <127457027+RenataSch@users.noreply.github.com>
2024-02-28 22:38:10 -0300

    Add files via upload

---
+++ EstruturaRepetitivaWhile/Exercicio2.java
@@ -0,0 +1,28 @@
+package EstruturaRepetitivaWhile;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int senha;
+		
+		System.out.println("Digite a senha de quatro dígitos:");
+		senha = sc.nextInt();
+		
+		while (senha != 2002) {
+			System.out.println("Senha Incorreta");
+			senha = sc.nextInt();
+		}
+		
+		System.out.println("\"Acesso Permitido");
+		
+
+	}
+
+}
